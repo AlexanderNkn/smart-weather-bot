707d20806bd48a83eadc6ab8f5a0581342df7805
makeitokay <andrey.mu11@yandex.ru>
2020-04-13 22:24:44 +0500

    PEP 8

--- bot/texts.py
+++ bot/texts.py
@@ -1,13 +1,13 @@
 MAIN_MENU_TEXT = "Now you can find out the current weather in your city or change your location."
 
 LOCATION_INPUT_TEXT = "Please, input your city name."
-LOCATION_CONFIRM_TEXT = "{}\nIs it your location?" # Country code and city name
+LOCATION_CONFIRM_TEXT = "{}\nIs it your location?"  # Country code and city name
 TRY_AGAIN_TEXT = "I can't understand your location. Please, try again."
-LOCATION_SET_TEXT = "Nice! Set your location - {}.\n" + MAIN_MENU_TEXT # City name
+LOCATION_SET_TEXT = "Nice! Set your location - {}.\n" + MAIN_MENU_TEXT  # City name
 
 TIME_INPUT_TEXT = "Ok, please input time you want to get weather information (use HH:MM format)"
 TIME_SET_TEXT = "Nice! I will send you weather every day at {} in {} timezone.\n" \
-                + MAIN_MENU_TEXT # time and timezone
+                + MAIN_MENU_TEXT  # time and timezone
 
 CORONAVIRUS_WARNING = """😷 And if possible, don't leave home!
 Self-isolation is the best way to win in this difficult situation in the world.
